385e5374db2dcfeb877b34e3113427afcd24a067
Poundps <poundokc@hotmail.com>
2021-09-22 07:19:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 function App() {
+  const [optionValue, setOptionValue] = React.useState("isPrime");
+  const [result, setResult] = React.useState("false");
+  const [number, setNum] = React.useState(0);
+  React.useEffect(() => {
+    const isFibanacci = (num) => {
+      if (isSquare(5 * (num * num) - 4) || isSquare(5 * (num * num) + 4)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    if (optionValue === "isPrime") {
+      isPrime(number) ? setResult("true") : setResult("false");
+    } else {
+      isFibanacci(number) ? setResult("true") : setResult("false");
+    }
+  }, [number, optionValue]);
+
+  var isSquare = function (num) {
+    return num > 0 && Math.sqrt(num) % 1 === 0;
+  };
+
+  const isPrime = (num) => {
+    for (let i = 2; i < num; i++) if (num % i === 0) return false;
+    return num > 1;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="row-wrap">
+      <div className="column1">
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => {
+            var target = e.target.value;
+            target < 0 ? setNum(1) : setNum(Math.round(target));
+          }}
+        />
+      </div>
+      <div className="column2">
+        <select
+          value={optionValue}
+          onChange={(e) => {
+            setOptionValue(e.target.value);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="isPrime">isPrime</option>
+          <option value="isFibonacci">isFibonacci</option>
+        </select>
+      </div>
+      <div className="column3">{result}</div>
     </div>
   );
 }
